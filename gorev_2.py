51450871c42713125ae39d16bb128c5e9516201b
Oğuzhan Arı <oguzhari@gmail.com>
2022-07-04 15:59:59 +0300

    reponun doğuşu

---
+++ gorev_2.py
@@ -0,0 +1,11 @@
+##########################################################
+# Görev 2: Verilen string ifadenin tüm harflerini büyük harfe
+# çeviriniz. Virgül ve nokta yerine space koyunuz, kelime kelime
+# ayırınız.
+
+# İpucu: String methodlarını kullanınız.
+##########################################################
+
+text = "The goal is to turn data into information, and information into sight"
+text_list = text.upper().split(" ")
+# Çıktı: ['THE', 'GOAL', 'IS', 'TO', 'TURN', 'DATA', 'INTO', 'INFORMATION,', 'AND', 'INFORMATION', 'INTO', 'SIGHT']
