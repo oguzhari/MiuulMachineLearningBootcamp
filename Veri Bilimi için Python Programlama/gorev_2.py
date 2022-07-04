5ee916b341e8bc51bcc7a6c0b10e05ab6db84929
Oğuzhan Arı <oguzhari@gmail.com>
2022-07-04 16:44:33 +0300

    Update gorev_2.py
    Ahmet Karayel ve Hasan Alperen Albayrak'ın önerdiği alternatif kodlar eklendi.

--- Veri Bilimi için Python Programlama/gorev_2.py
+++ Veri Bilimi için Python Programlama/gorev_2.py
@@ -7,5 +7,21 @@
 ##########################################################
 
 text = "The goal is to turn data into information, and information into sight"
-text_list = text.upper().split(" ")
+text = text.replace("."," ")
+text = text.replace(","," ")
+text = text.upper().split(" ")
 # Çıktı: ['THE', 'GOAL', 'IS', 'TO', 'TURN', 'DATA', 'INTO', 'INFORMATION,', 'AND', 'INFORMATION', 'INTO', 'SIGHT']
+
+
+#@Hasan Alperen Albayrak'ın Önerdiği Alternatif
+text.replace(".", " ").replace(",", " ").upper().split()
+#https://stackoverflow.com/questions/3411771/best-way-to-replace-multiple-characters-in-a-string
+
+
+#Ahmet Karayel'in Önerdiği Alternatif
+char_to_replace = {",": " ",
+                   ".": " "}
+
+for key, value in char_to_replace.items():
+  text = text.replace(key, value)
+text
